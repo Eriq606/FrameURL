855a3d2b25d87dae9a220b7b7a3c3462dca35faa
Eriq606 <erohweltall@gmail.com>
2023-03-07 10:20:05 +0300

    Ajout mappingUrl et modif mapping

--- etu1777/framework/servlet/FrontServlet.java
+++ etu1777/framework/servlet/FrontServlet.java
@@ -2,15 +2,18 @@ package etu1777.framework.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import etu1777.framework.Mapping;
 import utils.Utils;
 
 public class FrontServlet extends HttpServlet{
+    HashMap<String, Mapping> mappingUrls;
     public void processRequest(HttpServletRequest req, HttpServletResponse res) throws IOException{
         String url=req.getRequestURI();
         Utils utils=new Utils();
